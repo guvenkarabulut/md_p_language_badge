0ec0a32fc185bd5edafb0a8ea80cd2745a9203ee
guvenkarabulut <guven.07.568@gmail.com>
2023-12-25 16:40:56 -0500

    deploy configs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const getSvgRoutes = require('./src/routes/getSvgRoutes');
+// const getSvgRoutes = require('./src/routes/getSvgRoutes');
 const badgeRoutes = require('./src/routes/badgeRoutes');
 const app = express();
 const port = 3000;
